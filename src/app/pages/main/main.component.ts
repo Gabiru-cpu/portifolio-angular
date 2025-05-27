e2c50aaa5ca7dc4b861ec8f14d38748a8f9e84f6
Gabriel Librande <gabriel_librande@hotmail.com>
2025-05-27 13:37:50 -0300

    skill component implement

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { ExperienceComponent } from '../experience/experience.component';
+import { SkillsComponent } from '../skills/skills.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HomeComponent, ExperienceComponent],
+  imports: [HomeComponent, SkillsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
